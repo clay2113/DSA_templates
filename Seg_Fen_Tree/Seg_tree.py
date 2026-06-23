a9fcb24b0047af0828f7b4c851f614531da71c90
Nittin_S <114096437+clay2113@users.noreply.github.com>
2026-06-23 19:42:13 +0530

    add SegTreeMin class

--- Seg_Fen_Tree/Seg_tree.py
+++ Seg_Fen_Tree/Seg_tree.py
@@ -71,3 +71,41 @@ class SegTreeMax:
             l>>=1
             r>>=1
         return res
+
+
+
+
+"""MIN SEGMENT TREE"""    # FOR RANGE MINIMUM QUERIES + POINT UPDATES
+
+# Build: O(N)
+# Point update: O(logN)
+# Range min query: O(logN)
+
+class SegTreeMin:
+    #NittinS snippets
+    def __init__(self,a):
+        self.n=len(a)
+        self.seg=[float('inf')]*(2*self.n)
+        for i,x in enumerate(a):self.seg[self.n+i]=x
+        for i in range(self.n-1,0,-1):self.seg[i]=min(self.seg[i<<1],self.seg[i<<1|1])
+
+    def view(self):
+        return self.seg[self.n:]
+
+    def update(self,p,v):
+        p+=self.n
+        self.seg[p]=v
+        p>>=1
+        while p:
+            self.seg[p]=min(self.seg[p<<1],self.seg[p<<1|1])
+            p>>=1
+
+    def query(self,l,r):
+        l+=self.n;r+=self.n+1
+        res=float('inf')
+        while l<r:
+            if l&1:res=min(res,self.seg[l]);l+=1
+            if r&1:r-=1;res=min(res,self.seg[r])
+            l>>=1;r>>=1
+        return res
+        
