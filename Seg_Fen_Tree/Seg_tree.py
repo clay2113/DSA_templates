640d1dd241ce60e96e38f8b4b8ecf15ce5d2dc8a
Nittin_S <114096437+clay2113@users.noreply.github.com>
2026-06-23 22:55:20 +0530

    Refactor add lazy prop min updates

--- Seg_Fen_Tree/Seg_tree.py
+++ Seg_Fen_Tree/Seg_tree.py
@@ -108,4 +108,73 @@ class SegTreeMin:
             if r&1:r-=1;res=min(res,self.seg[r])
             l>>=1;r>>=1
         return res
+
+
+"""LAZY MIN SEGMENT TREE"""    # RANGE ADD + RANGE MIN QUERY
+
+# Build: O(N)
+# Range add: O(logN)
+# Range min query: O(logN)
+
+    class LazyMin:
+    #NittinS snippets
+        def __init__(self,a):
+            self.n=len(a)
+            self.seg=[0]*(4*self.n)
+            self.lazy=[0]*(4*self.n)
+            def build(i,l,r):
+                if l==r:
+                    self.seg[i]=a[l]
+                    return
+                m=(l+r)//2
+                build(i<<1,l,m)
+                build(i<<1|1,m+1,r)
+                self.seg[i]=min(self.seg[i<<1],self.seg[i<<1|1])
+            build(1,0,self.n-1)
+        def push(self,i):
+            if self.lazy[i]:
+                v=self.lazy[i]
+                self.seg[i<<1]+=v
+                self.seg[i<<1|1]+=v
+                self.lazy[i<<1]+=v
+                self.lazy[i<<1|1]+=v
+                self.lazy[i]=0
+        def add(self,l,r,v):
+            def upd(i,tl,tr):
+                if r<tl or tr<l:return
+                if l<=tl and tr<=r:
+                    self.seg[i]+=v
+                    self.lazy[i]+=v
+                    return
+                self.push(i)
+                tm=(tl+tr)//2
+                upd(i<<1,tl,tm)
+                upd(i<<1|1,tm+1,tr)
+                self.seg[i]=min(self.seg[i<<1],self.seg[i<<1|1])
+            upd(1,0,self.n-1)
+
+        def query(self,l,r):
+            def qry(i,tl,tr):
+                if r<tl or tr<l:return float('inf')
+                if l<=tl and tr<=r:
+                    return self.seg[i]
+                self.push(i)
+                tm=(tl+tr)//2
+                return min(
+                    qry(i<<1,tl,tm),
+                    qry(i<<1|1,tm+1,tr)
+                )
+            return qry(1,0,self.n-1)
+        def view(self):
+            res=[0]*self.n
+            def dfs(i,l,r):
+                if l==r:
+                    res[l]=self.seg[i]
+                    return
+                self.push(i)
+                m=(l+r)//2
+                dfs(i<<1,l,m)
+                dfs(i<<1|1,m+1,r)
+            dfs(1,0,self.n-1)
+            return res
         
