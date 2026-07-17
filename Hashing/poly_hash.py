08e38359105cb25dc204fba478ddc71c8f902366
Nittin_S <114096437+clay2113@users.noreply.github.com>
2026-07-17 12:17:18 +0530

    Add Rol_Hash64 class for polynomial rolling hashing
    Implemented a 64-bit polynomial rolling hash class for subarray equality checks, including methods for hashing and comparing subarrays.

---
+++ Hashing/poly_hash.py
@@ -0,0 +1,34 @@
+"""64-BIT POLYNOMIAL ROLLING HASH"""    # SUBARRAY EQUALITY
+
+# Build: O(N)
+# Subarray Hash: O(1)
+# Compare Subarrays: O(1)
+# Space: O(N)
+
+class Rol_Hash64:
+#NittinS Snippets
+    MASK=(1<<64)-1
+    @staticmethod
+    def splitmix64(x):
+        x&=RollingHash64.MASK
+        x=(x+0x9e3779b97f4a7c15)&RollingHash64.MASK
+        x=(x^(x>>30))*0xbf58476d1ce4e5b9&RollingHash64.MASK
+        x=(x^(x>>27))*0x94d049bb133111eb&RollingHash64.MASK
+        return (x^(x>>31))&RollingHash64.MASK
+
+    def __init__(self,a):
+        self.n=len(a)
+        self.BASE=(self.splitmix64(123456789)|1)
+        self.pow=[1]*(self.n+1)
+        self.pref=[0]*(self.n+1)
+        for i in range(self.n):
+            self.pow[i+1]=(self.pow[i]*self.BASE)&self.MASK
+            self.pref[i+1]=((self.pref[i]*self.BASE)+self.splitmix64(a[i]))&self.MASK
+
+    def hash(self,l,r):
+        return (self.pref[r+1]-(self.pref[l]*self.pow[r-l+1]))&self.MASK
+
+    def equal(self,l1,r1,l2,r2):
+        if r1-l1!=r2-l2:
+            return False
+        return self.hash(l1,r1)==self.hash(l2,r2)
