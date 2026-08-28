1241ee55ae79d3094f95c67519e7abebb0f5c786
Nittin S <114096437+clay2113@users.noreply.github.com>
2026-08-28 08:23:22 +0530

    Refactor isPrime function
    Refactor isPrime function for improved readability.

--- Math/primes.py
+++ Math/primes.py
@@ -23,21 +23,15 @@ def isPrime(n):
     #NittinS Snippets --> https://github.com/clay2113/DSA_templates
     if n<=1:
         return False
-
     if n==2 or n==3:
         return True
-
     if n%2==0 or n%3==0:
         return False
-
     i=5
-
     while i*i<=n:
         if n%i==0 or n%(i+2)==0:
             return False
-
         i+=6
-
     return True
 
 
