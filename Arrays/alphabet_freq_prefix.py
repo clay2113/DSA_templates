f99151e9171bb6d5db903b7befa90838a870c5d6
Nittin S <114096437+clay2113@users.noreply.github.com>
2026-09-18 06:43:50 +0530

    Rename AlphabetRange to AlphaRange and update comments
    Refactored AlphabetRange class to AlphaRange and updated method comments.

--- Arrays/alphabet_freq_prefix.py
+++ Arrays/alphabet_freq_prefix.py
@@ -1,25 +1,25 @@
 from collections import Counter
 
-class AlphabetRange:
-    # NittinS snippets
-    def __init__(self,s):
-        self.n=len(s)
-        self.pref=[[0]*26 for _ in range(self.n+1)]
-        for i,c in enumerate(s):
-            self.pref[i+1]=self.pref[i].copy()
-            self.pref[i+1][ord(c)-97]+=1
-    def __contains__(self,c):
-        # behaves like: c in s
-        x=ord(c)-97
-        return any(self.pref[self.n][x:x+1])
-    def freq(self,l,r):
-        # Counter of alphabets in s[l:r]
-        return Counter({
-            chr(i+97):self.pref[r][i]-self.pref[l][i]
-            for i in range(26)
-            if self.pref[r][i]-self.pref[l][i]
-        })
-    def count(self,c,l,r):
-        # frequency of c in s[l:r]
-        x=ord(c)-97
-        return self.pref[r][x]-self.pref[l][x]
+contains -->  # behaves like: c in s
+freq     -->  # Counter of alphabets in s[l:r]
+count    -->  # frequency of c in s[l:r]
+    class AlphaRange:
+        # NittinS snippets
+        def __init__(self,s):
+            self.n=len(s)
+            self.pref=[[0]*26 for _ in range(self.n+1)]
+            for i,c in enumerate(s):
+                self.pref[i+1]=self.pref[i].copy()
+                self.pref[i+1][ord(c)-97]+=1
+        def __contains__(self,c):
+            x=ord(c)-97
+            return any(self.pref[self.n][x:x+1])
+        def freq(self,l,r):
+            return Counter({
+                chr(i+97):self.pref[r][i]-self.pref[l][i]
+                for i in range(26)
+                if self.pref[r][i]-self.pref[l][i]
+            })
+        def count(self,c,l,r):
+            x=ord(c)-97
+            return self.pref[r][x]-self.pref[l][x]
