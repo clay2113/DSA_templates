79df0bdd7a0e3b1c67dc94ea3bf3e1813540f27a
Nittin S <114096437+clay2113@users.noreply.github.com>
2026-09-21 12:26:25 +0530

    Refactor StaticXor and StaticGcd classes
    Refactor StaticXor and StaticGcd classes. Update methods for range XOR and GCD queries.

--- Arrays/static_query.py
+++ Arrays/static_query.py
@@ -67,13 +67,34 @@ class StaticMin:
 
 
 
-"""STATIC XOR SPARSE TABLE"""    # RANGE XOR QUERY
+"""STATIC XOR"""    # RANGE XOR QUERY
 
-# Build: O(N log N)
-# Range xor query: O(log N)
-# Memory: O(N log N)
+# Build: O(N)
+# Range xor query: O(1)
 
 class StaticXor:
+    #NittinS snippets
+    def __init__(self,a):
+        self.pre=[0]
+
+        for x in a:
+            self.pre.append(self.pre[-1]^x)
+
+    def xor_val(self,l,r):
+        # inclusive [l,r]
+        return self.pre[r+1]^self.pre[l]
+
+
+
+"""STATIC GCD SPARSE TABLE"""    # RANGE GCD QUERY
+
+# Build: O(N log N)
+# Range gcd query: O(1)
+# Memory: O(N log N)
+
+from math import gcd
+
+class StaticGcd:
     #NittinS snippets
     def __init__(self,a):
         self.n=len(a)
@@ -87,7 +108,7 @@ class StaticXor:
             half=length>>1
 
             self.st.append([
-                prev[i]^prev[i+half]
+                gcd(prev[i],prev[i+half])
                 for i in range(self.n-length+1)
             ])
 
@@ -95,15 +116,14 @@ class StaticXor:
         for i in range(2,self.n+1):
             self.log[i]=self.log[i>>1]+1
 
-    def xor_val(self,l,r):
+    def gcd_val(self,l,r):
         # inclusive [l,r]
-        ans=0
+        k=self.log[r-l+1]
+        return gcd(
+            self.st[k][l],
+            self.st[k][r-(1<<k)+1]
+        )
 
-        for j in range(self.LOG-1,-1,-1):
-            length=1<<j
 
-            if length<=r-l+1:
-                ans^=self.st[j][l]
-                l+=length
 
-        return ans
+
