6198cf8c35328ccd57356e51d0bf35ae29e14027
Nittin S <114096437+clay2113@users.noreply.github.com>
2026-09-25 10:47:52 +0530

    Implement Bézout's lemma in number_theory.py
    Added Bézout's lemma explanation for GCD.

---
+++ Math/number_theory.py
@@ -0,0 +1,9 @@
+EUCLIDEAN ALGORITHM FOR GCD-->
+
+
+
+BEZOUTS LEMMA -->
+Bézout's identity, also known as Bézout's lemma, 
+states that for any non-zero integers \(a\) and \(b\),
+their greatest common divisor \(d = \gcd(a,b)\) 
+can always be expressed as a linear combination \(ax + by = d\) using integers \(x\) and \(y\)
